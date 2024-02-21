04cb9c72db5aaa80bc3670c8be18ed8e3c83ce5a
Jari Rentsch <jari.rentsch@jkweb.ch>
2024-02-21 09:34:10 +0100

    Add theme

---
+++ lib/config/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+    useMaterial3: true,
+  );
+}
