4ea946f8ec09dfa80d42b6150251248d69d27b67
Jari Rentsch <jari.rentsch@jkweb.ch>
2024-03-10 10:48:08 +0100

    Response success, error, exception

---
+++ lib/models/response.dart
@@ -0,0 +1,38 @@
+abstract class Response<T> {
+  T? getOrNull() => null;
+
+  String? getUiErrOrNull() => null;
+
+  bool get isSuccess => this is SuccessRes<T>;
+
+  bool get isFailure => this is ErrorRes || this is ExceptionRes;
+}
+
+class SuccessRes<T> extends Response<T> {
+  final T data;
+
+  SuccessRes(this.data);
+
+  @override
+  T? getOrNull() => data;
+}
+
+class ErrorRes<T> extends Response<T> {
+  final String errorMessage;
+  final String? uiMessage;
+
+  ErrorRes(this.errorMessage, {this.uiMessage});
+
+  @override
+  String? getUiErrOrNull() => uiMessage;
+}
+
+class ExceptionRes<T> extends Response<T> {
+  final Exception exception;
+  final String? uiMessage;
+
+  ExceptionRes(this.exception, {this.uiMessage});
+
+  @override
+  String? getUiErrOrNull() => uiMessage;
+}
