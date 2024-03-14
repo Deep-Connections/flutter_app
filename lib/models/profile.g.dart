1f55c1d9be8a062b25baab2c8a237073c779e85e
Jari Rentsch <jari.rentsch@jkweb.ch>
2024-03-14 18:53:42 +0100

    Automatic Serialization

---
+++ lib/models/profile.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Profile _$ProfileFromJson(Map<String, dynamic> json) => Profile(
+      name: json['name'] as String,
+      gender: json['gender'] as String,
+      genderLookingFor: (json['genderLookingFor'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      dateOfBirth: json['dateOfBirth'] == null
+          ? null
+          : DateTime.parse(json['dateOfBirth'] as String),
+      height: json['height'] as int,
+    );
+
+Map<String, dynamic> _$ProfileToJson(Profile instance) => <String, dynamic>{
+      'name': instance.name,
+      'gender': instance.gender,
+      'genderLookingFor': instance.genderLookingFor,
+      'dateOfBirth': instance.dateOfBirth?.toIso8601String(),
+      'height': instance.height,
+    };
