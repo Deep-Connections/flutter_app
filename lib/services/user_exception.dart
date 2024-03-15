10a554b9a0797c59fd8a3ab7bae97ff18eb136cf
Jari Rentsch <jari.rentsch@jkweb.ch>
2024-03-15 14:45:48 +0100

    Profile Firebase service

--- lib/services/user_exception.dart
+++ lib/services/user_exception.dart
@@ -2,6 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class UserNotFoundException extends FirebaseAuthException {
   UserNotFoundException(
-      {super.code = "User not logged in",
+      {super.code = "user_not_found",
       super.message = "Please login to use this feature."});
 }
