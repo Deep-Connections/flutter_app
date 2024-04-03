ad2af9f4c29f33ce3cacf9aa19c17b8ebad51f6e
Jari Rentsch <jarrentsch@gmail.com>
2024-04-03 20:27:17 +0200

    ChoiceQuestionWidget

---
+++ lib/screens/question/components/choice_question_widget.dart
@@ -0,0 +1,33 @@
+import 'package:deep_connections/models/question/choice_question.dart';
+import 'package:deep_connections/screens/profile/components/gender_button.dart';
+import 'package:deep_connections/screens/question/components/answer_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ChoiceQuestionWidget extends StatelessWidget {
+  final ChoiceQuestion question;
+  final AnswerNotifier answer;
+
+  const ChoiceQuestionWidget(
+      {super.key, required this.question, required this.answer});
+
+  @override
+  Widget build(BuildContext context) {
+    final loc = AppLocalizations.of(context);
+    return ListenableBuilder(
+      listenable: answer,
+      builder: (context, widget) {
+        return Column(
+          children: [
+            ...question.answers.map((a) => SelectableButton(
+                onPressed: () {
+                  answer.selectedAnswer = [a.value];
+                },
+                selected: answer.selectedAnswer.contains(a.value),
+                text: a.answerText.localize(loc)))
+          ],
+        );
+      },
+    );
+  }
+}
