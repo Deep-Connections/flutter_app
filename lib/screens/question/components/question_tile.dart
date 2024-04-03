0760aaa01172411cc2ccb0c239e965f0f511da33
Jari Rentsch <jarrentsch@gmail.com>
2024-04-03 08:36:29 +0200

    QuestionResponse model

--- lib/screens/question/components/question_tile.dart
+++ lib/screens/question/components/question_tile.dart
@@ -2,19 +2,24 @@ import 'package:deep_connections/models/question/question.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class QuestionWidget extends StatelessWidget {
+class QuestionWidget extends StatefulWidget {
   final Question question;
 
   const QuestionWidget({super.key, required this.question});
 
   @override
+  State<QuestionWidget> createState() => _QuestionWidgetState();
+}
+
+class _QuestionWidgetState extends State<QuestionWidget> {
+  @override
   Widget build(BuildContext context) {
     final loc = AppLocalizations.of(context);
     return Column(
       children: [
-        Text(question.questionText.localize(loc),
+        Text(widget.question.questionText.localize(loc),
             style: Theme.of(context).textTheme.headlineSmall),
-        ...question.answers.map((a) => ElevatedButton(
+        ...widget.question.answers.map((a) => ElevatedButton(
             onPressed: () {}, child: Text(a.answerText.localize(loc))))
       ],
     );
