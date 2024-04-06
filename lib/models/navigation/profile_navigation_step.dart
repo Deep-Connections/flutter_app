a226639ebc3dc1f084bab3c8f339fb151868aa0d
Jari Rentsch <jarrentsch@gmail.com>
2024-04-06 16:18:11 +0200

    Fix profile navigation

--- lib/models/navigation/profile_navigation_step.dart
+++ lib/models/navigation/profile_navigation_step.dart
@@ -2,6 +2,7 @@ import 'package:deep_connections/models/navigation/navigation_step.dart';
 import 'package:deep_connections/models/profile/profile/profile.dart';
 import 'package:deep_connections/screens/profile/birthday_profile_screen.dart';
 import 'package:deep_connections/screens/profile/gender/gender_profile_screen.dart';
+import 'package:deep_connections/screens/profile/gender_preferences/gender_preferences_profile_screen.dart';
 import 'package:deep_connections/screens/profile/name_profile_screen.dart';
 import 'package:deep_connections/services/profile/profile_service.dart';
 import 'package:flutter/cupertino.dart';
@@ -73,7 +74,7 @@ class GenderPreferencesProfileNavigationStep
 
   @override
   Widget createWidget(profileService, navigateToNext) {
-    return BirthdayProfileScreen(
+    return GenderPreferencesProfileScreen(
         profileService: profileService, navigateToNext: navigateToNext);
   }
 }
