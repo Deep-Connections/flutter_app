3173b992570f997950308e74b7f0939001daeb80
Sina <sinakeller2@gmail.com>
2024-04-07 16:46:59 +0200

    add more questions to profile

--- lib/models/profile/profile/profile.g.dart
+++ lib/models/profile/profile/profile.g.dart
@@ -30,6 +30,42 @@ _$ProfileImpl _$$ProfileImplFromJson(Map<String, dynamic> json) =>
           ? null
           : QuestionResponse.fromJson(
               json['question3'] as Map<String, dynamic>),
+      question4: json['question4'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question4'] as Map<String, dynamic>),
+      question5: json['question5'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question5'] as Map<String, dynamic>),
+      question6: json['question6'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question6'] as Map<String, dynamic>),
+      question7: json['question7'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question7'] as Map<String, dynamic>),
+      question8: json['question8'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question8'] as Map<String, dynamic>),
+      question9: json['question9'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question9'] as Map<String, dynamic>),
+      question10: json['question10'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question10'] as Map<String, dynamic>),
+      question11: json['question11'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question11'] as Map<String, dynamic>),
+      question12: json['question12'] == null
+          ? null
+          : QuestionResponse.fromJson(
+              json['question12'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$$ProfileImplToJson(_$ProfileImpl instance) {
@@ -50,5 +86,14 @@ Map<String, dynamic> _$$ProfileImplToJson(_$ProfileImpl instance) {
   writeNotNull('question1', instance.question1?.toJson());
   writeNotNull('question2', instance.question2?.toJson());
   writeNotNull('question3', instance.question3?.toJson());
+  writeNotNull('question4', instance.question4?.toJson());
+  writeNotNull('question5', instance.question5?.toJson());
+  writeNotNull('question6', instance.question6?.toJson());
+  writeNotNull('question7', instance.question7?.toJson());
+  writeNotNull('question8', instance.question8?.toJson());
+  writeNotNull('question9', instance.question9?.toJson());
+  writeNotNull('question10', instance.question10?.toJson());
+  writeNotNull('question11', instance.question11?.toJson());
+  writeNotNull('question12', instance.question12?.toJson());
   return val;
 }
