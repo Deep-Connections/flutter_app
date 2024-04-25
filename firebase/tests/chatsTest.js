a0c79201844c2b932fe9316a7b4ebcf493f315b2
Jari Rentsch <jarrentsch@gmail.com>
2024-04-25 19:28:16 +0200

    Firestore rules for chats

--- firebase/tests/chatsTest.js
+++ firebase/tests/chatsTest.js
@@ -8,17 +8,43 @@ const myAuth = { uid: UID, email: "example@gmail.com" };
 const wrongAuth = { uid: "wrong", email: "otheruser@gmail.com" };
 const chatId = "chat1";
 const participantIds = [UID, "otherUser"];
+const initialTimestamp = firebase.firestore.Timestamp.now();
+
+function getChatData() {
+    return {
+        participantIds,
+        timestamp: initialTimestamp,
+        lastMessage: {
+            senderId: UID,
+            text: "Initial message",
+            chatId: chatId,
+            timestamp: initialTimestamp
+        },
+        chatInfo: {
+            testUser: {
+                unreadMessages: 0,
+            },
+            otherUser: {
+                unreadMessages: 0,
+            }
+        }
+    };
+}
+
 
 describe("Chat functionality", () => {
 
-    beforeEach(async () => {
+    beforeEach(async () => { 
         await clearFirestoreData();
         // Use the admin app to bypass security rules and set up initial data
         const adminDb = getAdminFirestore();
-        await adminDb.collection(Collections.CHATS).doc(chatId).set({ participantIds });
+        await adminDb.collection(Collections.CHATS).doc(chatId).set({
+            participantIds,
+            timestamp: initialTimestamp
+        });
     });
 
-    afterEach(clearFirestoreData);
+    after(clearFirestoreData);
 
     it("can fetch a list of chats where the user is a participant", async () => {
         const db = getFirestore(myAuth);
@@ -36,6 +62,31 @@ describe("Chat functionality", () => {
         const query = chatsRef.doc(chatId);
         
         await firebase.assertFails(query.get());
+    });
+
+    it("allows updating a chat when the user is a participant and all fields are valid", async () => {
+        const db = getFirestore(myAuth);
+        const chatRef = db.collection(Collections.CHATS).doc(chatId);
+        const updateData = getChatData();
+
+        await firebase.assertSucceeds(chatRef.update(updateData));
+    });
+
+    it("forbids updates older than 1min than the previous update", async () => {
+        // more than 1min older
+        const tooOldTimestamp = new firebase.firestore.Timestamp.fromMillis(initialTimestamp.toMillis() - 60001);
+        const db = getFirestore(myAuth);
+        const chatRef = db.collection(Collections.CHATS).doc(chatId);
+        const updateData = getChatData();
+        updateData.timestamp = tooOldTimestamp;
+
+        await firebase.assertFails(chatRef.update(updateData));
+
+        // exactly 1min older
+        const exactlyOldTimestamp = new firebase.firestore.Timestamp.fromMillis(initialTimestamp.toMillis() - 60000);
+        updateData.timestamp = exactlyOldTimestamp;
+
+        await firebase.assertSucceeds(chatRef.update(updateData));
     });
 
 });
