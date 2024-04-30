d7fbb8f558faf6352e40933361003acffe88e716
Sina <sinakeller2@gmail.com>
2024-04-30 11:03:30 +0200

    fixed test

--- test/screens/question/choice_question_screen_test.dart
+++ test/screens/question/choice_question_screen_test.dart
@@ -17,10 +17,14 @@ void main() {
     id: '1',
     questionText: LocKey((loc) => loc.questionBasic_relationshipType_question),
     choices: [
-      Choice('1', LocKey((loc) => loc.questionBasic_relationshipType_answer1)),
-      Choice('2', LocKey((loc) => loc.questionBasic_relationshipType_answer2)),
-      Choice('3', LocKey((loc) => loc.questionBasic_relationshipType_answer3)),
-      Choice('4', LocKey((loc) => loc.questionBasic_relationshipType_answer4)),
+      Choice('1',
+          LocKey((loc) => loc.questionBasic_relationshipType_answer_oneNight)),
+      Choice('2',
+          LocKey((loc) => loc.questionBasic_relationshipType_answer_short)),
+      Choice(
+          '3', LocKey((loc) => loc.questionBasic_relationshipType_answer_long)),
+      Choice(
+          '4', LocKey((loc) => loc.questionBasic_relationshipType_answer_life)),
     ],
     navigationPath: '',
     section: ProfileSection.basic,
@@ -56,14 +60,15 @@ void main() {
     }
 
     // Initially 3 should be selected and 1 should not be selected
-    checkSelected(loc.questionBasic_relationshipType_answer3, true);
-    checkSelected(loc.questionBasic_relationshipType_answer1, false);
+    checkSelected(loc.questionBasic_relationshipType_answer_long, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_oneNight, false);
 
     // Select answer 1 and check if 3 is not selected anymore
-    await tester.tap(find.text(loc.questionBasic_relationshipType_answer1));
+    await tester
+        .tap(find.text(loc.questionBasic_relationshipType_answer_oneNight));
     await tester.pumpAndSettle();
-    checkSelected(loc.questionBasic_relationshipType_answer1, true);
-    checkSelected(loc.questionBasic_relationshipType_answer3, false);
+    checkSelected(loc.questionBasic_relationshipType_answer_oneNight, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_long, false);
     // the complete_profile should still contain 3
     checkQuestion(['3']);
     await tester.tap(find.text(loc.general_next));
@@ -77,10 +82,14 @@ void main() {
     id: '2',
     questionText: LocKey((loc) => loc.questionBasic_relationshipType_question),
     choices: [
-      Choice('1', LocKey((loc) => loc.questionBasic_relationshipType_answer1)),
-      Choice('2', LocKey((loc) => loc.questionBasic_relationshipType_answer2)),
-      Choice('3', LocKey((loc) => loc.questionBasic_relationshipType_answer3)),
-      Choice('4', LocKey((loc) => loc.questionBasic_relationshipType_answer4)),
+      Choice('1',
+          LocKey((loc) => loc.questionBasic_relationshipType_answer_oneNight)),
+      Choice('2',
+          LocKey((loc) => loc.questionBasic_relationshipType_answer_short)),
+      Choice(
+          '3', LocKey((loc) => loc.questionBasic_relationshipType_answer_long)),
+      Choice(
+          '4', LocKey((loc) => loc.questionBasic_relationshipType_answer_life)),
     ],
     minChoices: 2,
     maxChoices: 3,
@@ -113,39 +122,42 @@ void main() {
     }
 
     // Initially 2 should be selected and 1 should not be selected
-    checkSelected(loc.questionBasic_relationshipType_answer2, true);
-    checkSelected(loc.questionBasic_relationshipType_answer1, false);
-    checkSelected(loc.questionBasic_relationshipType_answer3, false);
+    checkSelected(loc.questionBasic_relationshipType_answer_short, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_oneNight, false);
+    checkSelected(loc.questionBasic_relationshipType_answer_long, false);
 
     // As we have a previous answer, the next button should be enabled,
     // even though we have not selected the minimum number of answers
     tester.checkButtonEnabled(loc.general_next, enabled: true);
 
     // Select answer 1 and check that both are selected
-    await tester.tap(find.text(loc.questionBasic_relationshipType_answer1));
+    await tester
+        .tap(find.text(loc.questionBasic_relationshipType_answer_oneNight));
     await tester.pumpAndSettle();
-    checkSelected(loc.questionBasic_relationshipType_answer1, true);
-    checkSelected(loc.questionBasic_relationshipType_answer2, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_oneNight, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_short, true);
     // the complete_profile should still contain 2
     checkQuestion(['2']);
 
     // unselect 2 and check that next button is disabled
-    await tester.tap(find.text(loc.questionBasic_relationshipType_answer2));
+    await tester
+        .tap(find.text(loc.questionBasic_relationshipType_answer_short));
     await tester.pumpAndSettle();
     tester.checkButtonEnabled(loc.general_next, enabled: false);
 
     // select all 3 answers
-    await tester.tap(find.text(loc.questionBasic_relationshipType_answer2));
-    await tester.tap(find.text(loc.questionBasic_relationshipType_answer3));
+    await tester
+        .tap(find.text(loc.questionBasic_relationshipType_answer_short));
+    await tester.tap(find.text(loc.questionBasic_relationshipType_answer_long));
     await tester.pumpAndSettle();
-    await tester.tap(find.text(loc.questionBasic_relationshipType_answer4));
+    await tester.tap(find.text(loc.questionBasic_relationshipType_answer_life));
     await tester.pumpAndSettle();
 
     // check that all answers are selected except for 4, as we can only select 3
-    checkSelected(loc.questionBasic_relationshipType_answer1, true);
-    checkSelected(loc.questionBasic_relationshipType_answer2, true);
-    checkSelected(loc.questionBasic_relationshipType_answer3, true);
-    checkSelected(loc.questionBasic_relationshipType_answer4, false);
+    checkSelected(loc.questionBasic_relationshipType_answer_oneNight, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_short, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_long, true);
+    checkSelected(loc.questionBasic_relationshipType_answer_life, false);
 
     await tester.tap(find.text(loc.general_next));
     await tester.pumpAndSettle();
