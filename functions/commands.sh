a2f472215baa2432433645eacd41bc15d24af3e9
Jari Rentsch <jarrentsch@gmail.com>
2024-05-01 10:34:50 +0200

    Initial match function implementation

---
+++ functions/commands.sh
@@ -0,0 +1,10 @@
+
+// debugging
+npm run dev
+
+// then go to chrome and open the following link
+//chrome://inspect/#devices
+
+// click on inspect and you will see the console
+
+// run your function and it will stop at the breakpoint
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,60 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+export const createInitialMatch = functions.https.onRequest( async (req, res) => {
+    debugger;
+
+    const userId = req.query.userId; // Or pass via POST body
+
+    if (!userId || typeof userId !== 'string') {
+        res.status(400).send('User ID is required');
+        return;
+    }
+
+    try {
+        const profile = await admin.firestore().collection('profiles').doc(userId).get();
+        
+        const data = profile.data();
+
+
+        if (!data) {
+            res.status(404).send('Profile not found');
+            return;
+        }
+
+        if (data.numMatches >= 1) {
+            res.status(400).send('Match already exists');
+            return;
+        }
+
+        // todo find similar profiles
+        
+        const otherProfile = await admin.firestore().collection('profiles').limit(1).get();
+
+        if (otherProfile.empty) {
+            res.status(404).send('No profiles found');
+            return;
+        }
+
+        const otherUserId = otherProfile.docs[0].id;
+
+        const match = {
+            participantIds: [userId, otherUserId],
+            createdAt: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        await admin.firestore().collection('chats').add(match);
+        await admin.firestore().collection('profiles').doc(userId).update({ numMatches: admin.firestore.FieldValue.increment(1) });
+        await admin.firestore().collection('profiles').doc(otherUserId).update({ numMatches: admin.firestore.FieldValue.increment(1) });
+
+        res.status(201).send('Match created');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error creating match');
+    }
+});
+
