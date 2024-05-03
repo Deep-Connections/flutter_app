ada856ce6cda174ce7d35529661cbaada2888e02
Jari Rentsch <jarrentsch@gmail.com>
2024-05-03 16:30:42 +0200

    createInitialMatch write more tests

--- functions/tests/index.test.js
+++ functions/tests/index.test.js
@@ -16,29 +16,74 @@ require('sinon').stub(admin, 'initializeApp');
 const createInitialMatch = test.wrap(require('../index').createInitialMatch)
 
 const firebase = require('@firebase/testing');
+const fs = require('fs');
 
-describe('createInitialMatch', () => {
+const UID = 'test-uid';
+
+const context = {
+    auth: {
+        uid: UID,
+        token: {}
+    }
+};
+
+function convertProfileFirebase(profile, numMatches = undefined) {
+    const birthDate = new Date(profile.birthdate);
+    const asFirebaseDate = admin.firestore.Timestamp.fromDate(birthDate);
+    profile.birthdate = asFirebaseDate;
+    return profile;
+}
+
+function storeMyProfile() {
+    const profileData = JSON.parse(fs.readFileSync('../scripts/generated/single_profile.json', 'utf8'));
+    return admin.firestore().collection('profiles').doc(UID).set(convertProfileFirebase(profileData));
+}
+
+function storeProfiles(numProfiles = 100, numMatches = 0) {
+    const multipleProfilesData = JSON.parse(fs.readFileSync('../scripts/generated/multiple_profile.json', 'utf8')).slice(0, numProfiles);
+    return Promise.all(multipleProfilesData.map((profile) => {
+        profile.numMatches = numMatches;
+        admin.firestore().collection('profiles').add(convertProfileFirebase(profile));
+    }));
+}
+
+async function hasNoMatch() {
+    try {
+        await createInitialMatch({}, context);
+        assert.fail('Should have thrown an error');
+    } catch (e) {
+        assert.equal(e.message, 'No profiles found');
+    }
+}
+
+describe('InitialMatch', () => {
 
     beforeEach(async () => {
-        firebase.clearFirestoreData({ projectId });
+        await firebase.clearFirestoreData({ projectId });
     });
 
-    it('should create a match successfully', async () => {
-        await admin.firestore().collection('profiles').doc('test-uid').set({ numMatches: 0 });
-        await admin.firestore().collection('profiles').doc('other-user-id').set({ numMatches: 0 });
-        const context = {
-            auth: {
-                uid: 'test-uid',
-                token: {}
-            }
-        };
+    it('can create successfully', async () => {
+        await storeMyProfile();
+        await storeProfiles();
 
-        try {
-            const result = await createInitialMatch({}, context);
-            assert.equal(result.message, 'Match created');
-        } catch (error) {
-            throw new Error(`Function threw an error: ${error.message}`);
-        }
+        const result = await createInitialMatch({}, context);
+        assert.equal(result.message, 'Match created');
+        const match = await admin.firestore().collection('matches').doc(result.matchId).get();
+        assert.deepEqual(match.data().participantIds[0], UID);
+    });
+
+    it('should not find yourself', async () => {
+        await storeMyProfile();
+        await hasNoMatch();
+    });
+
+    it('should not find people that already have 5 matches', async () => {
+        await storeMyProfile();
+        await storeProfiles(1, numMatches = 5);
+        await hasNoMatch();
+        await storeProfiles(1, numMatches = 4);
+        const result = await createInitialMatch({}, context);
+        assert.equal(result.message, 'Match created');
     });
 });
 
