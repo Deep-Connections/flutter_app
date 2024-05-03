7b2371d4573fecb0755d207a17f580b366d18c03
Jari Rentsch <jarrentsch@gmail.com>
2024-05-03 18:28:25 +0200

    createInitialMatch Compute matching score based on questions and return the one with the highest score

--- functions/index.js
+++ functions/index.js
@@ -26,6 +26,43 @@ async function findPotentialMatches(profileData, userId) {
         .limit(100).get();
 }
 
+function computeMatchScore(profile1, profile2) {
+    let score = 0;
+    try {
+        for (const key in profile1.questions) {
+            try {
+                const array1 = profile1.questions[key].response;
+                const array2 = profile2.questions[key].response;
+                let subScore = 0;
+                for (const element of array1) {
+                    if (array2.includes(element)) {
+                        subScore++;
+                    }
+                }
+                score += subScore * 2 / (array1.length + array2.length);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    return score;
+}
+
+function sortProfilesByMatchScore(profiles, user) {
+    const profilesWithScores = profiles.map(profile => ({
+        profile,
+        score: computeMatchScore(user, profile)
+    }));
+
+    //console.log(profilesWithScores.map(item => item.profile.firstName + ", score: " + item.score));
+
+    profilesWithScores.sort((a, b) => b.score - a.score);
+
+    return profilesWithScores;
+}
+
 exports.createInitialMatch = functions.https.onCall(async (data, context) => {
     if (!context.auth || !context.auth.uid) {
         throw new functions.https.HttpsError('unauthenticated', 'The function must be called while authenticated.');
@@ -46,15 +83,19 @@ exports.createInitialMatch = functions.https.onCall(async (data, context) => {
     // }
 
     const profiles = await findPotentialMatches(currentProfile, userId);
-    const numMatches = profiles.docs.map((profile) => profile.data().numMatches);
     if (profiles.empty) {
         throw new functions.https.HttpsError('not-found', 'No profiles found');
     }
 
-    const otherUserId = profiles.docs[0].id;
+    profilesWithScores = sortProfilesByMatchScore(profiles.docs.map(doc => {
+        return { id: doc.id, ...doc.data() }
+    }), currentProfile);
+
+    const otherUserId = profilesWithScores[0].profile.id;
     const match = {
         participantIds: [userId, otherUserId],
-        createdAt: admin.firestore.FieldValue.serverTimestamp()
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        score: profilesWithScores[0].score
     };
 
     const matchDocRef = db.collection('matches').doc();
