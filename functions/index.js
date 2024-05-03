dfdefef508b532780c94f6e57748a53269c2a559
Jari Rentsch <jarrentsch@gmail.com>
2024-05-03 16:30:25 +0200

    createInitialMatch filter by num matches and own userId

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,27 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+const ageDifference = 5;
+
+const db = admin.firestore();
+
+async function findPotentialMatches(profileData, userId) {
+    const birthdate = profileData.birthdate.toDate();
+    const fiveYearsOlder = new Date(birthdate);
+    fiveYearsOlder.setFullYear(fiveYearsOlder.getFullYear() + ageDifference);
+    const fiveYearsYounger = new Date(birthdate);
+    fiveYearsYounger.setFullYear(fiveYearsYounger.getFullYear() - ageDifference);
+
+    return await db.collection('profiles')
+        .where("languageCodes", 'array-contains-any', profileData.languageCodes)
+        .where("birthdate", '<=', fiveYearsOlder)
+        .where("birthdate", '>=', fiveYearsYounger)
+        .where(admin.firestore.FieldPath.documentId(), '!=', userId)
+        .where("numMatches", '<', 5)
+        .orderBy("numMatches", "desc")
+        .limit(100).get();
+}
+
 exports.createInitialMatch = functions.https.onCall(async (data, context) => {
     if (!context.auth || !context.auth.uid) {
         throw new functions.https.HttpsError('unauthenticated', 'The function must be called while authenticated.');
@@ -13,19 +34,19 @@ exports.createInitialMatch = functions.https.onCall(async (data, context) => {
     const userId = context.auth.uid;
 
 
-    const profileRef = admin.firestore().collection('profiles').doc(userId);
-    const profileSnapshot = await profileRef.get();
-    const profileData = profileSnapshot.data();
+    const profileRef = db.collection('profiles').doc(userId);
+    const currentProfile = (await profileRef.get()).data();
 
-    if (!profileData) {
-        throw new functions.https.HttpsError('not-found', 'Profile not found');
+    if (!currentProfile) {
+        throw new functions.https.HttpsError('not-found', 'Current user\'s profile not found');
     }
 
-    if (profileData.numMatches >= 1) {
-        throw new functions.https.HttpsError('failed-precondition', 'Match already exists');
-    }
+    // if (currentProfile.numMatches >= 1) {
+    //     throw new functions.https.HttpsError('failed-precondition', 'Match already exists');
+    // }
 
-    const profiles = await admin.firestore().collection('profiles').limit(1).get();
+    const profiles = await findPotentialMatches(currentProfile, userId);
+
     if (profiles.empty) {
         throw new functions.https.HttpsError('not-found', 'No profiles found');
     }
@@ -36,11 +57,13 @@ exports.createInitialMatch = functions.https.onCall(async (data, context) => {
         createdAt: admin.firestore.FieldValue.serverTimestamp()
     };
 
-    await admin.firestore().collection('chats').add(match);
+    const matchDocRef = db.collection('matches').doc();
+    const matchId = matchDocRef.id;
+    await matchDocRef.set(match);
     await profileRef.update({ numMatches: admin.firestore.FieldValue.increment(1) });
-    await admin.firestore().collection('profiles').doc(otherUserId).update({ numMatches: admin.firestore.FieldValue.increment(1) });
+    await db.collection('profiles').doc(otherUserId).update({ numMatches: admin.firestore.FieldValue.increment(1) });
 
-    return { message: 'Match created' };
+    return { message: 'Match created', matchId: matchId };
 
 });
 
