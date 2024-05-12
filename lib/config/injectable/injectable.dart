a000c5a1dc92c47ab723250ac5f361ac0c849b60
Jari Rentsch <jarrentsch@gmail.com>
2024-05-12 11:51:51 +0200

    Make sure by default we don't run debug mode on emulator. Only if the FIREBASE_EMULATOR env var is set.

--- lib/config/injectable/injectable.dart
+++ lib/config/injectable/injectable.dart
@@ -1,9 +1,24 @@
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
 import 'injectable.config.dart';
 
 final getIt = GetIt.instance;
+
+String getEnvironment() {
+  if (kReleaseMode) return Environment.prod;
+
+  // to run with the emulator: flutter run --dart-define=FIREBASE_EMULATOR=true
+  String? isFirebaseEmulator =
+      const String.fromEnvironment('FIREBASE_EMULATOR');
+
+  if (isFirebaseEmulator == "true") {
+    return Environment.dev;
+  } else {
+    return Environment.prod;
+  }
+}
 
 @InjectableInit()
 void configureDependencies(String environment) =>
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,11 @@ import 'package:deep_connections/firebase_options.dart';
 import 'package:deep_connections/navigation/graphs/main_nav_graph.dart';
 import 'package:deep_connections/services/utils/error_handling.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
-import 'package:injectable/injectable.dart';
 
 import 'config/constants.dart';
 import 'config/injectable/injectable.dart';
@@ -18,7 +16,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  configureDependencies(/*kDebugMode ? Environment.dev :*/ Environment.prod);
+
+  configureDependencies(getEnvironment());
 
   // url routing
   usePathUrlStrategy();
