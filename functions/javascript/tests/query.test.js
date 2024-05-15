ea3814cf6e2cf59e8f817dca7d95a7392c97fba9
Jari Rentsch <jarrentsch@gmail.com>
2024-05-15 11:31:12 +0200

    Refactor functions structure

--- functions/javascript/tests/query.test.js
+++ functions/javascript/tests/query.test.js
@@ -2,7 +2,7 @@
 const admin = require("firebase-admin");
 const { Collections } = require("../constants");
 
-const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = require("../../../serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
