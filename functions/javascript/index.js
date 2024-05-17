ffdde4dbe4256a758fc4a780046bd436c6d1955e
Jari Rentsch <jarrentsch@gmail.com>
2024-05-17 12:33:24 +0200

    Implement unmatch cloud function

--- functions/javascript/index.js
+++ functions/javascript/index.js
@@ -4,107 +4,12 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const { FieldValue } = require("firebase-admin/firestore");
 const { Collections, FunctionErrors } = require("./constants");
-const { AllQuestions } = require("./generated_flutter_constants");
 
 admin.initializeApp();
 
-const ageDifference = 5;
-const maxNumMatches = 5;
-
 const db = admin.firestore();
 
-async function getPotentialMatches(profileData, userId) {
-  const dateOfBirth = profileData.dateOfBirth.toDate();
-  const fiveYearsOlder = new Date(dateOfBirth);
-  fiveYearsOlder.setFullYear(fiveYearsOlder.getFullYear() + ageDifference);
-  const fiveYearsYounger = new Date(dateOfBirth);
-  fiveYearsYounger.setFullYear(fiveYearsYounger.getFullYear() - ageDifference);
-  const matchedUserIds = (profileData.matchedUserIds || []);
-
-  let profiles = (await db.collection(Collections.PROFILES)
-      .where("numMatches", "<", maxNumMatches)
-      .where("gender", "in", profileData.genderPreferences.slice(0, 10))
-      .where("genderPreferences", "array-contains", profileData.gender)
-      .where("dateOfBirth", "<=", fiveYearsOlder)
-      .where("dateOfBirth", ">=", fiveYearsYounger)
-      .orderBy("numMatches", "asc")
-      .limit(100).get()).docs.filter((doc) => doc.id !== userId);
-
-  profiles = profiles.filter((profile) =>
-    profile.data().languageCodes.some((language) => profileData.languageCodes.includes(language)));
-
-  if (profiles.filter((doc) => !matchedUserIds.includes(doc.id)).length === 0) {
-    // To make sure we find a new user, search for more users than we have already matched with
-    const limit = matchedUserIds.length + 2;
-    profiles = (await db.collection(Collections.PROFILES)
-        .where("numMatches", ">=", 0) // to exclude unfinished profiles that don't have numMatches
-        .where("gender", "in", profileData.genderPreferences.slice(0, 10))
-        .where("genderPreferences", "array-contains", profileData.gender)
-        .orderBy("numMatches", "asc")
-        .limit(limit).get()).docs.filter((doc) => doc.id !== userId);
-
-    if (profiles.filter((doc) => !matchedUserIds.includes(doc.id).length) === 0) {
-      db.collection(Collections.PROFILES).doc(userId).update({ numMatches: 0 });
-      throw new functions.https.HttpsError(FunctionErrors.NOT_FOUND, "No matching profiles found");
-    }
-  }
-
-  return profiles;
-}
-
-function isObject(value) {
-  return value && typeof value === "object" && value.constructor === Object;
-}
-
-function isNumber(value) {
-  return typeof value === "number" && !isNaN(value);
-}
-
-function computeMatchScore(profile1, profile2) {
-  let score = 0;
-  if (isObject(profile1.questions) && isObject(profile2.questions)) {
-    for (const id of AllQuestions) {
-      if (Object.hasOwn(profile1.questions, id) && Object.hasOwn(profile2.questions, id)) {
-        try {
-          const confidence1 = profile1.questions[id].confidence;
-          const confidence2 = profile2.questions[id].confidence;
-          if (isNumber(confidence1) && isNumber(confidence2)) {
-            score += 0.5 - Math.abs(confidence1 - confidence2);
-            continue;
-          }
-          const choices1 = profile1.questions[id].choices;
-          const choices2 = profile2.questions[id].choices;
-          if (Array.isArray(choices1) && Array.isArray(choices2)) {
-            let subScore = 0;
-            for (const choice of choices1) {
-              if (choices2.includes(choice)) {
-                subScore++;
-              }
-            }
-            score += subScore * 2 / (choices1.length + choices2.length);
-            continue;
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      }
-    }
-    return score;
-  }
-}
-
-function sortProfilesByMatchScore(profiles, user) {
-  const profilesWithScores = profiles.map((profile) => ({
-    profile,
-    score: computeMatchScore(user, profile),
-  }));
-
-  profilesWithScores.sort((a, b) => b.score - a.score);
-
-  // console.log(profilesWithScores.map((item) => item.profile.firstName + ", score: " + item.score));
-
-  return profilesWithScores;
-}
+const { getPotentialMatches, sortProfilesByMatchScore } = require("./matchComputation");
 
 exports.createInitialMatch = functions.region("europe-west6").https.onCall(async (data, context) => {
   if (!context.auth || !context.auth.uid) {
@@ -155,7 +60,7 @@ exports.createInitialMatch = functions.region("europe-west6").https.onCall(async
     }
   });
 
-  const profiles = await getPotentialMatches(currentProfile, userId);
+  const profiles = await getPotentialMatches(db, currentProfile, userId);
 
   const profilesWithScores = sortProfilesByMatchScore(profiles.map((doc) => {
     return { id: doc.id, ...doc.data() };
@@ -175,6 +80,7 @@ exports.createInitialMatch = functions.region("europe-west6").https.onCall(async
   const match = {
     participantIds: [userId, matchUserId],
     createdAt: FieldValue.serverTimestamp(),
+    lastUpdatedAt: FieldValue.serverTimestamp(),
     score: profilesWithScores[0].score,
   };
 
@@ -194,3 +100,54 @@ exports.createInitialMatch = functions.region("europe-west6").https.onCall(async
   return { message: "Match created", matchId: matchId };
 });
 
+exports.unmatch = functions.region("europe-west6").https.onCall(async (data, context) => {
+  if (!context.auth || !context.auth.uid) {
+    throw new functions.https.HttpsError(
+        FunctionErrors.UNAUTHENTICATED,
+        "The function must be called while authenticated.");
+  }
+
+  const userId = context.auth.uid;
+  const chatId = data.chatId;
+
+  if (!chatId) {
+    throw new functions.https.HttpsError(FunctionErrors.INVALID_ARGUMENT, "chatId is required");
+  }
+
+  const profileDoc = await db.collection(Collections.PROFILES).doc(userId).get();
+  if (!profileDoc.exists || !profileDoc.data().firstName) {
+    throw new functions.https.HttpsError(FunctionErrors.NOT_FOUND, "User not found");
+  }
+  const firstName = profileDoc.data().firstName;
+  await db.runTransaction(async (transaction) => {
+    const chatRef = db.collection(Collections.CHATS).doc(chatId);
+    const chatDoc = await transaction.get(chatRef);
+    if (!chatDoc.exists) {
+      throw new functions.https.HttpsError(FunctionErrors.NOT_FOUND, "Chat not found");
+    }
+
+    const chatData = chatDoc.data();
+    if (!chatData.participantIds.includes(userId)) {
+      throw new functions.https.HttpsError(FunctionErrors.PERMISSION_DENIED, "User is not a participant in this chat");
+    }
+
+    transaction.update(chatRef, {
+      participantIds: FieldValue.arrayRemove(userId),
+      lastUpdatedAt: FieldValue.serverTimestamp(),
+    });
+    transaction.update(db.collection(Collections.PROFILES).doc(userId), {
+      numMatches: FieldValue.increment(-1),
+    });
+    const messageRef = chatRef.collection(Collections.MESSAGES);
+    // create a message to indicate that the user has unmatched
+    transaction.create(messageRef.doc(), {
+      chatId: chatId,
+      runtimeType: "unmatch",
+      createdAt: FieldValue.serverTimestamp(),
+      lastUpdatedAt: FieldValue.serverTimestamp(),
+      participantIds: [userId],
+      senderFirstName: firstName,
+      senderId: userId,
+    });
+  });
+});
---
+++ functions/javascript/tests/generateProfiles.js
@@ -0,0 +1,15 @@
+
+const admin = require("firebase-admin");
+const projectId = "deep-connections-7796d";
+process.env.GCLOUD_PROJECT = "deep-connections-7796d";
+process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+admin.initializeApp({ projectId });
+const { storeProfiles } = require("./mockProfile");
+
+storeProfiles(10, []).then(() => {
+  console.log("Profiles created");
+}).catch((e) => {
+  console.error(e);
+});
+
+// node tests/generateProfiles.js
