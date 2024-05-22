83994130fcabfd6c69b73560d6b367d9be7c8256
Jari Rentsch <jarrentsch@gmail.com>
2024-05-22 09:47:33 +0200

    Photo grid UI to delete images

--- lib/screens/profile/components/photo_grid.dart
+++ lib/screens/profile/components/photo_grid.dart
@@ -1,12 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:deep_connections/config/constants.dart';
 import 'package:deep_connections/models/profile/picture/picture.dart';
+import 'package:deep_connections/screens/components/dialogs/confirmation_dialog.dart';
 import 'package:deep_connections/utils/extensions/general_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_reorderable_grid_view/entities/order_update_entity.dart';
 import 'package:flutter_reorderable_grid_view/widgets/widgets.dart';
 
-const maxNumberOfPhotos = 6;
+const maxNumPhotos = 6;
 
 class PhotoGrid extends StatefulWidget {
   final List<Picture> photoUrls;
@@ -25,7 +27,7 @@ class PhotoGrid extends StatefulWidget {
 }
 
 class _PhotoGridState extends State<PhotoGrid> {
-  late final List<Picture?> _photoUrls = [];
+  final List<Picture?> _photoUrls = [];
   final _scrollController = ScrollController();
   final _gridViewKey = GlobalKey();
 
@@ -49,10 +51,19 @@ class _PhotoGridState extends State<PhotoGrid> {
     }
   }
 
+  _submitPhotos() {
+    final newPhotos = _photoUrls.mapNotNull((e) => e).toList();
+    if (newPhotos.isNotEmpty) {
+      widget.submitNewPhotos(newPhotos);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final numMissingPhotos = maxNumberOfPhotos - _photoUrls.length;
+    final numMissingPhotos = maxNumPhotos - _photoUrls.length;
     final photoUrlsAndNull = _photoUrls + List.filled(numMissingPhotos, null);
+    final colorScheme = Theme.of(context).colorScheme;
+    final loc = AppLocalizations.of(context);
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(standardPadding),
@@ -61,8 +72,7 @@ class _PhotoGridState extends State<PhotoGrid> {
           dragChildBoxDecoration: BoxDecoration(
             boxShadow: [
               BoxShadow(
-                color:
-                    Theme.of(context).colorScheme.onBackground.withOpacity(0.2),
+                color: colorScheme.onBackground.withOpacity(0.2),
                 spreadRadius: 4,
                 blurRadius: 8.0,
                 offset: const Offset(0, 3), // changes position of shadow
@@ -73,10 +83,7 @@ class _PhotoGridState extends State<PhotoGrid> {
             for (final orderUpdateEntity in orderUpdateEntities) {
               final photo = _photoUrls.removeAt(orderUpdateEntity.oldIndex);
               _photoUrls.insert(orderUpdateEntity.newIndex, photo);
-              final newPhotos = _photoUrls.mapNotNull((e) => e).toList();
-              if (newPhotos.isNotEmpty) {
-                widget.submitNewPhotos(newPhotos);
-              }
+              _submitPhotos();
             }
           },
           lockedIndices: List.generate(
@@ -100,17 +107,21 @@ class _PhotoGridState extends State<PhotoGrid> {
                     key: Key(index.toString()),
                     onTap: widget.addPhoto,
                     child: Container(
-                      color: Colors.grey[200],
-                      child: const Icon(Icons.add),
+                      color: colorScheme.surface,
+                      child: Icon(Icons.add, color: colorScheme.onSurface),
                     ),
                   );
                 } else {
                   final url = photo.url!;
-                  return CachedNetworkImage(
-                    key: Key(url),
-                    imageUrl: url,
-                    fit: BoxFit.cover,
-                  );
+                  return PhotoItem(
+                      key: Key(url),
+                      url: url,
+                      onDelete: () {
+                        setState(() {
+                          _photoUrls.removeAt(index);
+                          //_submitPhotos();
+                        });
+                      });
                 }
               })
               .map((child) => ClipRRect(
@@ -120,6 +131,49 @@ class _PhotoGridState extends State<PhotoGrid> {
               .toList(),
         ),
       ),
+    );
+  }
+}
+
+class PhotoItem extends StatelessWidget {
+  final String url;
+  final void Function() onDelete;
+
+  const PhotoItem({super.key, required this.url, required this.onDelete});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final loc = AppLocalizations.of(context);
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        CachedNetworkImage(
+          imageUrl: url,
+          fit: BoxFit.cover,
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: IconButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(colorScheme.surface),
+            ),
+            icon: Icon(Icons.delete, color: colorScheme.onSurface),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return ConfirmationDialog(
+                        context: context,
+                        titleText: loc.profilePhotos_deleteTitle,
+                        contentText: loc.profilePhotos_deleteContent,
+                        confirmText: loc.general_delete,
+                        onConfirm: onDelete);
+                  });
+            },
+          ),
+        )
+      ],
     );
   }
 }
