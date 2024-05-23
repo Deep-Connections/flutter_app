0e404f09b4a53d2d4f2e46ee1b61d67684f86614
Jari Rentsch <jarrentsch@gmail.com>
2024-05-23 21:27:16 +0200

    create deleted message in chats of user

--- functions/javascript/src/cloud_functions/deleteAccount.js
+++ functions/javascript/src/cloud_functions/deleteAccount.js
@@ -1,53 +1,83 @@
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const { FieldValue } = require("firebase-admin/firestore");
 const { Collections, FieldName, FunctionErrors } = require("../constants");
 const { chunkArray } = require("../helpers");
-
 const db = admin.firestore();
+
 const auth = admin.auth();
 const bucket = admin.storage().bucket();
 
-async function deleteAccountByUserId(userId) {
-  let allMessagesOfUser = db.collectionGroup(Collections.MESSAGES)
+async function deleteMessages(userId) {
+  const allMessagesOfUser = await db.collectionGroup(Collections.MESSAGES)
       .where(FieldName.SENDER_ID, "==", userId).select().get();
 
-  const allChatsOfUser = await db.collection(Collections.CHATS)
-      .where(FieldName.PARTICIPANT_IDS, "array-contains", userId).get();
-
-  allMessagesOfUser = await allMessagesOfUser;
-
-  const deleteMessagesPromises = chunkArray(allMessagesOfUser.docs, 500).map((chunk) => {
+  return Promise.all(chunkArray(allMessagesOfUser.docs, 500).map((chunk) => {
     const batch = db.batch();
     chunk.forEach((doc) => batch.delete(doc.ref));
     return batch.commit();
+  }));
+}
+
+async function deleteChats(userId, profilePromise) {
+  const allChatsOfUser = await db.collection(Collections.CHATS)
+      .where(FieldName.PARTICIPANT_IDS, "array-contains", userId).get();
+  const firstName = await profilePromise.then((doc) => {
+    if (!doc.exists || !doc.data().firstName) {
+      return null;
+    }
+    return doc.data().firstName;
   });
 
-  const removeChatParticipantPromises = chunkArray(allChatsOfUser.docs, 500).map((chunk) => {
-    chunk.forEach((doc) => {
+  return Promise.all(chunkArray(allChatsOfUser.docs, 250).map((chunk) => {
+    chunk.forEach((chat) => {
       const batch = db.batch();
-      if (doc.data().participantIds.length === 1) {
-        batch.delete(doc.ref);
+      if (chat.data().participantIds.length === 1) {
+        batch.delete(chat.ref);
       } else {
-        batch.update(doc.ref, {
+        batch.update(chat.ref, {
           participantIds: admin.firestore.FieldValue.arrayRemove(userId),
           lastUpdatedAt: admin.firestore.FieldValue.serverTimestamp(),
         });
+        if (firstName !== null) {
+          batch.create(db.collection(Collections.CHATS).doc(chat.id).collection(Collections.MESSAGES).doc(), {
+            chatId: chat.id,
+            runtimeType: "delete",
+            createdAt: FieldValue.serverTimestamp(),
+            lastUpdatedAt: FieldValue.serverTimestamp(),
+            participantIds: chat.data().participantIds.filter((id) => id !== userId),
+            senderFirstName: firstName,
+            senderId: userId,
+          });
+        }
       }
       return batch.commit();
     });
-  });
+  }));
+}
 
-  const deleteProfilePromise = db.collection(Collections.PROFILES).doc(userId).delete();
+async function deleteAccountByUserId(userId) {
+  const profilePromise = db.collection(Collections.PROFILES).doc(userId).get();
+
+  const deleteMessagesPromise = deleteMessages(userId);
+
+  const deleteChatsPromise = deleteChats(userId, profilePromise);
 
   const deletUserPromise = auth.deleteUser(userId);
 
   const deletFilesPromise = bucket.deleteFiles({ prefix: `profile_images/${userId}/` });
 
-  await Promise.all([deleteProfilePromise,
+  await profilePromise;
+  const deleteProfilePromise = db.collection(Collections.PROFILES).doc(userId).delete();
+
+  await Promise.all([
+    deleteProfilePromise,
     deletUserPromise,
     deletFilesPromise,
-  ] + deleteMessagesPromises + removeChatParticipantPromises);
+    deleteMessagesPromise,
+    deleteChatsPromise,
+  ]);
 }
 
 exports.deleteAccountByUserId = deleteAccountByUserId;
--- functions/javascript/tests/deleteAccount.test.js
+++ functions/javascript/tests/deleteAccount.test.js
@@ -52,16 +52,13 @@ async function deleteUser(userId) {
   } catch (e) {/* ignore */}
 }
 
-async function createMockUserData(userId, otherUserId) {
+async function createMockUserData(userId) {
   await Promise.all([
     auth.createUser({ uid: userId }),
     bucket.file(`profile_images/${userId}/image.jpg`).save("test"),
     db.collection(Collections.PROFILES).doc(userId).set({
       firstName: userId,
     }),
-    // create a chat with another user, and a chat with only the to be deleted user
-    createChat([userId, otherUserId]),
-    createChat([userId]),
   ]);
 }
 
@@ -76,7 +73,11 @@ describe("Delete account", () => {
   });
 
   it("removes all sensitive data of user", async () => {
-    await createMockUserData(UID_1, UID_2);
+    await Promise.all([
+      createMockUserData(UID_1),
+      createChat([UID_1, UID_2]),
+      createChat([UID_1]),
+    ]);
     await deleteAccount({}, { auth: { uid: UID_1 } });
 
     const allChatsPromise = db.collection(Collections.CHATS).get();
@@ -84,19 +85,18 @@ describe("Delete account", () => {
     const profilePromise = db.collection(Collections.PROFILES).doc(UID_1).get();
     const filesPromise = bucket.getFiles({ prefix: `profile_images/${UID_1}/` });
 
-    const allMessages = await allMessagesPromise;
-    const allChats = await allChatsPromise;
-    // Message from the other user remains
-    assert.equal(allMessages.size, 1);
-    assert.equal(allMessages.docs.filter((doc) => doc.data().senderId === UID_1).length, 0);
+    // account deleted message is only remaining message of user1
+    const userMessages = (await allMessagesPromise).docs.filter((doc) => doc.data().senderId === UID_1);
+    assert.equal(userMessages.length, 1);
+    assert.equal(userMessages[0].data().runtimeType, "delete");
+    assert.equal(userMessages[0].data().senderFirstName, UID_1);
 
-    // the chat with the other user remains
+    // the chat with the other user remains, account where user1 is alone is deleted
+    const allChats = await allChatsPromise;
     assert.equal(allChats.size, 1);
     assert.equal(allChats.docs.filter((doc) => doc.data().participantIds.includes(UID_1)).length, 0);
 
-    // check image is deleted
     assert.equal((await filesPromise)[0].length, 0);
-
     assert.equal((await profilePromise).exists, false);
     try {
       await admin.auth().getUser(UID_1);
@@ -113,19 +113,29 @@ describe("Delete account", () => {
 
   it("should leave the other user's data intact", async () => {
     await Promise.all([
-      createMockUserData(UID_1, UID_2),
-      createMockUserData(UID_2, UID_1),
+      createMockUserData(UID_1),
+      createMockUserData(UID_2),
+      createChat([UID_1, UID_2]), // contains 1 message of each user
+      createChat([UID_2]), // contains 2 messages of user2
+      createChat([UID_1]),
     ]);
     await deleteAccount({}, { auth: { uid: UID_1 } });
 
     const profile2 = db.collection(Collections.PROFILES).doc(UID_2).get();
-    const constChatsOfUser2 = db.collection(Collections.CHATS).where("participantIds", "array-contains", UID_2).get();
-    const constMessagesOfUser2 = db.collectionGroup(Collections.MESSAGES).where("senderId", "==", UID_2).get();
+    const chatsOfUser2 = db.collection(Collections.CHATS).get();
+    const messagesPromise = db.collectionGroup(Collections.MESSAGES).get();
     const user2 = admin.auth().getUser(UID_2);
     const imagesOfUser2 = bucket.getFiles({ prefix: `profile_images/${UID_2}/` });
-    assert.equal((await profile2).data().firstName, UID_2);
-    assert.equal((await constChatsOfUser2).size, 3);
-    assert.equal((await constMessagesOfUser2).size, 4);
+
+    const messages = await messagesPromise;
+    const user1SentMessages = messages.docs.filter((doc) => doc.data().senderId === UID_1);
+    const user2ReceivedMessages = messages.docs.filter((doc) => doc.data().participantIds.includes(UID_2));
+    assert.equal((await profile2).exists, true);
+    assert.equal((await chatsOfUser2).size, 2);
+    // user2 should have the 3 previous messages and a deleted message
+    assert.equal(user2ReceivedMessages.length, 4);
+    assert.equal(user1SentMessages.length, 1);
+    assert.equal(user1SentMessages[0].data().runtimeType, "delete");
     assert.equal((await user2).uid, UID_2);
     assert.equal((await imagesOfUser2)[0].length, 1);
   });
